c9e0c9c3170723447e85355747ff5364e304a5f3
Dogu Tas <tocersdeck@gmail.com>
2021-06-11 20:41:01 +0300

    Death of the OOP

--- src/GPSTools.cpp
+++ src/GPSTools.cpp
@@ -1,19 +1,13 @@
-#include "GPSTools.h"
 #include<math.h>
 
-// GPSTools::GPSTools()
-// {
-// 	
-// }
-
-GPSTools::CalcDistance(double lat1, double long1, double lat2, double long2)
+double CalcDistance(double lat1, double long1, double lat2, double long2)
 {
 	double sin1 = sin((lat1 - lat2) / 2);
 	double sin2 = sin((long1 - long2) / 2);
 	return (2 * asin((sin1 * sin1) + (cos(lat1) * cos(lat2) * sin2 * sin2)));
 }
 
-GPSTools::CalcHeading(double lat1, double long1, double lat2, double long2)
+double CalcHeading(double lat1, double long1, double lat2, double long2)
 {
 	double atan_ = atan((sin(long1 - long2) * cos(lat1) * cos(lat2) * sin(lat2)) - (cos(long1 - long2) * sin(lat1) * cos(lat2)));
 	return (atan_ * atan_);
--- src/GPSTools.h
+++ src/GPSTools.h
@@ -1,14 +1,7 @@
-#ifndef _GPSTools_H /* start of the OSAB.h */
+#ifndef _GPSTools_H /* start of the GPSTools.h */
 #define _GPSTools_H
 
-class GPSTools
-{
-	public:
-// 		GPSTools();
-		CalcHeading(double lat1, double long1, double lat2, double long2);
-		CalcDistance(double lat1, double long1, double lat2, double long2);
-	private:
-    
-};
+double CalcHeading(double lat1, double long1, double lat2, double long2);
+double CalcDistance(double lat1, double long1, double lat2, double long2);
 
 #endif /* _GPSTools_H */
